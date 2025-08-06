32559e822cb9af06e1d0a134fafc4c1f9cb28fab
Damian D'Souza <dsouzadamian189@gmail.com>
2025-08-06 22:47:51 +0200

    add util functions to install packages and flatpaks

---
+++ setup/fedora/utils.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+install_flatpaks() {
+    local flatpaks=("$@")
+    for app in "${flatpaks[@]}"; do
+        echo "Installing $app"
+        flatpak install --noninteractive "$app"
+    done
+}
+
+install_packages() {
+    local packages=("$@")
+    echo "Installing dnf packages"
+    sudo dnf install -y "${packages[@]}"
+}
