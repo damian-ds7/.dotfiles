1233012ba059eff0319a98ec80a42cb217c44ea3
Damian D'Souza <dsouzadamian189@gmail.com>
2025-08-07 20:46:37 +0200

    add utility functions for gnome keybindings and list of keybindings to add

--- setup/fedora/utils.sh
+++ setup/fedora/utils.sh
@@ -13,3 +13,44 @@ install_packages() {
     echo "Installing dnf packages"
     sudo dnf install -y --skip-unavailable "${packages[@]}"
 }
+
+# =======================================
+# === Gnome related utility functions ===
+# =======================================
+
+apply_overrides() {
+    local arr=("$@")
+    local schema key value
+
+    for item in "${arr[@]}"; do
+        read -r schema key value <<< "$item"
+        gsettings set "$schema" "$key" "$value"
+    done
+}
+
+apply_custom_shortcuts() {
+    local arr=("$@")
+    local base_path="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
+    local paths=()
+    local i=0
+
+    for item in "${arr[@]}"; do
+        IFS='|' read -r name cmd binding <<< "$item"
+        local path="$base_path/custom$i/"
+        local full_path="org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$path"
+
+        gsettings set "$full_path" name "$name"
+        gsettings set "$full_path" command "$cmd"
+        gsettings set "$full_path" binding "$binding"
+
+        paths+=("'$path'")
+        ((i++))
+    done
+
+    local joined_paths
+    IFS=,
+    joined_paths="${paths[*]}"
+    unset IFS
+
+    gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[$joined_paths]"
+}
