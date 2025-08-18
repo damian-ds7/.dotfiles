ff2e05d59c6bd61578dc72aca12447001104dc6d
Damian D'Souza <dsouzadamian189@gmail.com>
2025-08-18 22:29:22 +0200

    move tmux reset to toggle script, refactor script a bit

--- scripts/toggle-dark-mode.sh
+++ scripts/toggle-dark-mode.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env sh
 set -euo pipefail
 
-if test "$(gsettings get org.gnome.desktop.interface color-scheme)" = "'prefer-light'"; then
+TMUX_THEME_DIR="$HOME/.tmux/themes"
+
+get_current_theme() {
+    gsettings get org.gnome.desktop.interface color-scheme
+}
+
+switch_to_dark() {
     gsettings set org.gnome.desktop.interface color-scheme prefer-dark
     gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
-    ln -sf ~/.tmux/themes/dark.conf ~/.tmux/themes/current-theme.conf
-else
+    "$TMUX_THEME_DIR/reset.sh"
+    ln -sf "$TMUX_THEME_DIR/dark.conf" "$TMUX_THEME_DIR/current-theme.conf"
+}
+
+switch_to_light() {
     gsettings set org.gnome.desktop.interface color-scheme prefer-light
     gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
-    ln -sf ~/.tmux/themes/light.conf ~/.tmux/themes/current-theme.conf
+    "$TMUX_THEME_DIR/reset.sh"
+    ln -sf "$TMUX_THEME_DIR/light.conf" "$TMUX_THEME_DIR/current-theme.conf"
+}
+
+# Main logic
+if test "$(get_current_theme)" = "'prefer-light'"; then
+    switch_to_dark
+else
+    switch_to_light
 fi
 
 tmux source-file ~/.tmux.conf
