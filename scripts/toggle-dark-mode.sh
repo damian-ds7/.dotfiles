4c2685ad8483cf02b86cf2a326bafec05ee2b975
Damian D'Souza <dsouzadamian189@gmail.com>
2025-08-18 21:27:52 +0200

    add tmux theme switch

--- scripts/toggle-dark-mode.sh
+++ scripts/toggle-dark-mode.sh
@@ -4,7 +4,11 @@ set -euo pipefail
 if test "$(gsettings get org.gnome.desktop.interface color-scheme)" = "'prefer-light'"; then
     gsettings set org.gnome.desktop.interface color-scheme prefer-dark
     gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
+    ln -sf ~/.tmux/themes/dark.conf ~/.tmux/themes/current-theme.conf
 else
     gsettings set org.gnome.desktop.interface color-scheme prefer-light
     gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
+    ln -sf ~/.tmux/themes/light.conf ~/.tmux/themes/current-theme.conf
 fi
+
+tmux source-file ~/.tmux.conf
