f05d04cc78204483c488fdeb8aaac0e65ae8f5f3
Damian D'Souza <dsouzadamian189@gmail.com>
2025-08-18 19:21:39 +0200

    add lazygit to setup

--- setup/fedora/setup.sh
+++ setup/fedora/setup.sh
@@ -71,7 +71,7 @@ setup_repos() {
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
-    sudo dnf copr enable dejan/lazygit
+    sudo dnf copr enable -y dejan/lazygit
 
     dnf check-update
 }
