3c68872829c84b1d9bd3fd9ff32cefac01e386f0
Damian D'Souza <dsouzadamian189@gmail.com>
2025-08-19 19:05:50 +0200

    add util y function for yazi

--- home/.shellrc.d/common/aliases.sh
+++ home/.shellrc.d/common/aliases.sh
@@ -134,6 +134,14 @@ xopen() {
   xdg-open "$arg"
 }
 
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+
 # -------------------------
 #  Systemd Helpers
 # -------------------------
