b3b977472939f64860eaf8727c6d05566166cf4d
Damian D'Souza <dsouzadamian189@gmail.com>
2025-08-19 02:18:51 +0200

    switch lsd theme to script

--- scripts/.local/bin/toggle-dark-mode.sh
+++ scripts/.local/bin/toggle-dark-mode.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 TMUX_THEME_DIR="$HOME/.tmux/themes"
+LSD_THEME_DIR="$HOME/.config/lsd"
 
 get_current_theme() {
     gsettings get org.gnome.desktop.interface color-scheme
@@ -12,6 +13,7 @@ switch_to_dark() {
     gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
     "$TMUX_THEME_DIR/reset.sh"
     ln -sf "$TMUX_THEME_DIR/dark.conf" "$TMUX_THEME_DIR/current-theme.conf"
+    ln -sf  "$LSD_THEME_DIR/dark.yaml" "$LSD_THEME_DIR/colors.yaml"
 }
 
 switch_to_light() {
@@ -19,9 +21,9 @@ switch_to_light() {
     gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
     "$TMUX_THEME_DIR/reset.sh"
     ln -sf "$TMUX_THEME_DIR/light.conf" "$TMUX_THEME_DIR/current-theme.conf"
+    ln -sf  "$LSD_THEME_DIR/light.yaml" "$LSD_THEME_DIR/colors.yaml"
 }
 
-# Main logic
 if test "$(get_current_theme)" = "'prefer-light'"; then
     switch_to_dark
 else
