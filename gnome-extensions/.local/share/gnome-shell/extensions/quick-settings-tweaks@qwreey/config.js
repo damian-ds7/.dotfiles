4db05402fe0800bf8dd4e68472ac485cfa09dedd
Damian D'Souza <dsouzadamian189@gmail.com>
2025-08-20 17:14:22 +0200

    remove gnome extensions

--- gnome-extensions/.local/share/gnome-shell/extensions/quick-settings-tweaks@qwreey/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    isDevelopmentBuild: false,
-    isReleaseBuild: true,
-    isGithubBuild: false,
-    version: "2.2",
-    buildNumber: 9,
-    baseGTypeName: "quick-settings-tweaks_",
-    loggerPrefix: "[quick-settings-tweaks]",
-};
