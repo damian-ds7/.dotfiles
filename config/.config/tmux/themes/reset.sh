add4b7978cc9f188e00428318cdb2dc7b235f4bf
Damian D'Souza <dsouzadamian189@gmail.com>
2025-09-12 20:15:08 +0200

    fix tmux config location

--- config/.config/tmux/themes/reset.sh
+++ config/.config/tmux/themes/reset.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 # Unset options
 rg -Io 'set\s+-[aFgopqsuUw]+\s+"?@([^\s]+(\w|_))"?' -r '@$1' $HOME/.config/tmux/plugins/tmux/**/*.conf | uniq | xargs -n1 -P0 tmux set -Ugq
 
-status_dir="$HOME/.tmux/plugins/tmux/status"
-status_utils_file="$HOME/.tmux/plugins/tmux/utils/status_module.conf"
+status_dir="$HOME/.config/tmux/plugins/tmux/status"
+status_utils_file="$HOME/.config/tmux/plugins/tmux/utils/status_module.conf"
 
 # Get all status modules in $HOME/.config/tmux/plugins/tmux/status/
 modules=()
