6be6313bdb86aa0fc2ff5825d6ea55e4ce03f1c3
Damian D'Souza <dsouzadamian189@gmail.com>
2025-09-25 16:42:27 +0200

    add fuzzel theme to theme toggle script

--- scripts/.local/bin/toggle-dark-mode.sh
+++ scripts/.local/bin/toggle-dark-mode.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 TMUX_CONF_DIR="$HOME/.config/tmux"
 TMUX_THEME_DIR="$TMUX_CONF_DIR/themes"
 LSD_THEME_DIR="$HOME/.config/lsd"
+FUZZEL_THEME_DIR="$HOME/.config/fuzzel/themes"
 ULAUNCHER_SETTINGS="$HOME/.config/ulauncher/settings.json"
 
 get_current_theme() {
@@ -15,6 +16,7 @@ switch_to_dark() {
     dconf write /org/gnome/desktop/interface/gtk-theme "'adw-gtk3-dark'"
     "$TMUX_THEME_DIR/reset.sh"
     ln -sf "$TMUX_THEME_DIR/dark.conf" "$TMUX_THEME_DIR/current-theme.conf"
+    ln -sf "$FUZZEL_THEME_DIR/dark.ini" "$FUZZEL_THEME_DIR/current-theme.ini"
     ln -sf  "$LSD_THEME_DIR/dark.yaml" "$LSD_THEME_DIR/colors.yaml"
     sed -i 's/"theme-name": "ulauncher-theme-gnome-light"/"theme-name": "ulauncher-theme-gnome-dark"/' "$ULAUNCHER_SETTINGS"
     systemctl --user restart ulauncher
@@ -25,6 +27,7 @@ switch_to_light() {
     dconf write /org/gnome/desktop/interface/gtk-theme "'adw-gtk3'"
     "$TMUX_THEME_DIR/reset.sh"
     ln -sf "$TMUX_THEME_DIR/light.conf" "$TMUX_THEME_DIR/current-theme.conf"
+    ln -sf "$FUZZEL_THEME_DIR/light.ini" "$FUZZEL_THEME_DIR/current-theme.ini"
     ln -sf  "$LSD_THEME_DIR/light.yaml" "$LSD_THEME_DIR/colors.yaml"
     sed -i 's/"theme-name": "ulauncher-theme-gnome-dark"/"theme-name": "ulauncher-theme-gnome-light"/' "$ULAUNCHER_SETTINGS"
     systemctl --user restart ulauncher
