c5b630009d8ad1f9d875aab9103b2b872a85f0e3
Damian D'Souza <dsouzadamian189@gmail.com>
2025-11-13 21:43:53 +0100

    make theme links have relative path

--- scripts/.local/bin/toggle-dark-mode.sh
+++ scripts/.local/bin/toggle-dark-mode.sh
@@ -8,37 +8,37 @@ NIRI_CONF_DIR="$HOME/.config/niri"
 ULAUNCHER_SETTINGS="$HOME/.config/ulauncher/settings.json"
 
 get_current_theme() {
-    dconf read /org/gnome/desktop/interface/color-scheme
+  dconf read /org/gnome/desktop/interface/color-scheme
 }
 
 switch_to_dark() {
-    dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
-    dconf write /org/gnome/desktop/interface/gtk-theme "'adw-gtk3-dark'"
-    qs ipc call darkMode setDark
-    vicinae vicinae://theme/set/catppuccin-mocha
-    "$TMUX_THEME_DIR/reset.sh"
-    ln -sfn "$THEME_DIR/catppuccin-mocha"  "$THEME_DIR/current"
-    sed -i 's/^\([[:space:]]*active-color[[:space:]]\+\)"#[^"]*"/\1"#cba6f7"/' "$NIRI_CONF_DIR/config.kdl"
-    sed -i 's/"theme-name": "ulauncher-theme-gnome-light"/"theme-name": "ulauncher-theme-gnome-dark"/' "$ULAUNCHER_SETTINGS"
-    systemctl --user try-restart ulauncher
+  dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+  dconf write /org/gnome/desktop/interface/gtk-theme "'adw-gtk3-dark'"
+  qs ipc call darkMode setDark
+  vicinae vicinae://theme/set/catppuccin-mocha
+  "$TMUX_THEME_DIR/reset.sh"
+  ln -sfnr "$THEME_DIR/catppuccin-mocha" "$THEME_DIR/current"
+  sed -i 's/^\([[:space:]]*active-color[[:space:]]\+\)"#[^"]*"/\1"#cba6f7"/' "$NIRI_CONF_DIR/config.kdl"
+  sed -i 's/"theme-name": "ulauncher-theme-gnome-light"/"theme-name": "ulauncher-theme-gnome-dark"/' "$ULAUNCHER_SETTINGS"
+  systemctl --user try-restart ulauncher
 }
 
 switch_to_light() {
-    dconf write /org/gnome/desktop/interface/color-scheme "'prefer-light'"
-    dconf write /org/gnome/desktop/interface/gtk-theme "'adw-gtk3'"
-    qs ipc call darkMode setLight
-    vicinae vicinae://theme/set/catppuccin_latte
-    "$TMUX_THEME_DIR/reset.sh"
-    ln -sfn "$THEME_DIR/catppuccin-latte"  "$THEME_DIR/current"
-    sed -i 's/^\([[:space:]]*active-color[[:space:]]\+\)"#[^"]*"/\1"#8839ef"/' "$NIRI_CONF_DIR/config.kdl"
-    sed -i 's/"theme-name": "ulauncher-theme-gnome-dark"/"theme-name": "ulauncher-theme-gnome-light"/' "$ULAUNCHER_SETTINGS"
-    systemctl --user try-restart ulauncher
+  dconf write /org/gnome/desktop/interface/color-scheme "'prefer-light'"
+  dconf write /org/gnome/desktop/interface/gtk-theme "'adw-gtk3'"
+  qs ipc call darkMode setLight
+  vicinae vicinae://theme/set/catppuccin_latte
+  "$TMUX_THEME_DIR/reset.sh"
+  ln -sfnr "$THEME_DIR/catppuccin-latte" "$THEME_DIR/current"
+  sed -i 's/^\([[:space:]]*active-color[[:space:]]\+\)"#[^"]*"/\1"#8839ef"/' "$NIRI_CONF_DIR/config.kdl"
+  sed -i 's/"theme-name": "ulauncher-theme-gnome-dark"/"theme-name": "ulauncher-theme-gnome-light"/' "$ULAUNCHER_SETTINGS"
+  systemctl --user try-restart ulauncher
 }
 
 if test "$(get_current_theme)" = "'prefer-light'"; then
-    switch_to_dark
+  switch_to_dark
 else
-    switch_to_light
+  switch_to_light
 fi
 
 systemctl --user try-restart waybar.service
