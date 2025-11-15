d2bed5ef40394c12a42f3b68920bad072aec23f0
Damian D'Souza <dsouzadamian189@gmail.com>
2025-11-15 15:49:22 +0100

    make folds in nvim persist across session

--- config/.config/nvim/lua/config/options.lua
+++ config/.config/nvim/lua/config/options.lua
@@ -1,3 +1,23 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Make folds persist across sessions
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local save_fold = augroup("Persistent Folds", { clear = true })
+
+autocmd("BufWinLeave", {
+  pattern = "*.*",
+  callback = function()
+    vim.cmd.mkview()
+  end,
+  group = save_fold,
+})
+autocmd("BufWinEnter", {
+  pattern = "*.*",
+  callback = function()
+    vim.cmd.loadview({ mods = { emsg_silent = true } })
+  end,
+  group = save_fold,
+})
