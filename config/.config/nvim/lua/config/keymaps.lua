10aaca8b40656d989721c5546f6a760efedd8ab5
Damian D'Souza <dsouzadamian189@gmail.com>
2025-11-15 15:49:47 +0100

    add extra bindings for nvim

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Jump half page down and center view" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Jump half page up and center view" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Jump half page down and center view" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Jump half page up and center view" })
+
+map("n", "n", "'Nn'[v:searchforward].'zzzv'", { expr = true, desc = "Next Search Result" })
+map("n", "N", "'nN'[v:searchforward].'zzzv'", { expr = true, desc = "Prev Search Result" })
