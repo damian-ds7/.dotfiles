ae601e3753ec1455161988ade75da5079b1f0d6f
Damian D'Souza <dsouzadamian189@gmail.com>
2025-11-29 00:09:10 +0100

    feat(nvim): add keybinds for changing cwd in telescope

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -8,3 +8,23 @@ map("n", "<C-u>", "<C-u>zz", { desc = "Jump half page up and center view" })
 
 map("n", "n", "'Nn'[v:searchforward].'zzzv'", { expr = true, desc = "Next Search Result" })
 map("n", "N", "'nN'[v:searchforward].'zzzv'", { expr = true, desc = "Prev Search Result" })
+
+map("n", "<leader>ko", function()
+  require("telescope").extensions.file_browser.file_browser({
+    path = vim.fn.getcwd(),
+    cwd = vim.fn.getcwd(),
+    hidden = true,
+    grouped = true,
+    initial_mode = "insert",
+  })
+end, { desc = "Change directory with file browser" })
+
+map("n", "<leader>kO", function()
+  require("telescope").extensions.file_browser.file_browser({
+    path = vim.fn.expand("~"),
+    cwd = vim.fn.expand("~"),
+    hidden = true,
+    grouped = true,
+    initial_mode = "insert",
+  })
+end, { desc = "Change directory from home" })
