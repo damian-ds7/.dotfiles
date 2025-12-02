58c0672710752924a5f8bcfbc963df55c285d26b
Damian D'Souza <dsouzadamian189@gmail.com>
2025-12-02 11:24:29 +0100

    feat(nvim): new lualine config

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        component_separators = "",
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+        lualine_z = {
+          {
+            function()
+              return " " .. os.date("%R")
+            end,
+            separator = { right = "" },
+          },
+        },
+      },
+    },
+  },
+}
