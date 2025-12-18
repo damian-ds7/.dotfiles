6e4bccb65ea24c5b3af7cb8657f0543c545362dd
Damian D'Souza <dsouzadamian189@gmail.com>
2025-12-18 22:55:47 +0100

    feat(nvim): add sshfs plugin

---
+++ nvim/.config/nvim/lua/plugins/sshfs.lua
@@ -0,0 +1,21 @@
+return {
+  "uhs-robert/sshfs.nvim",
+  opts = {
+    connections = {
+      sshfs_options = {
+        reconnect = true,
+        ConnectTimeout = 5,
+        ServerAliveInterval = 15,
+        ServerAliveCountMax = 3,
+        dir_cache = "yes",
+        dcache_timeout = 300,
+        dcache_max_size = 10000,
+      },
+      control_persist = "10m",
+      socket_dir = vim.fn.expand("$HOME/.ssh/sockets"),
+    },
+    mounts = {
+      base_dir = vim.fn.expand("$HOME") .. "/ssh-mnt",
+    },
+  },
+}
