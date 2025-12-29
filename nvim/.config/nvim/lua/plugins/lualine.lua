d31fc3569d03322bed388b23b36bc28393f365b9
Damian D'Souza <dsouzadamian189@gmail.com>
2025-12-29 22:24:37 +0100

    style: cleanup

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,20 +1,18 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-      options = {
-        component_separators = "",
-        section_separators = { left = "", right = "" },
-      },
-      sections = {
-        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
-        lualine_z = {
-          {
-            function()
-              return " " .. os.date("%R")
-            end,
-            separator = { right = "" },
-          },
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    options = {
+      component_separators = "",
+      section_separators = { left = "", right = "" },
+    },
+    sections = {
+      lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+      lualine_z = {
+        {
+          function()
+            return " " .. os.date("%R")
+          end,
+          separator = { right = "" },
         },
       },
     },
