cd8984f47c733671c9055b7239ced60055bb73fd
Damian D'Souza <dsouzadamian189@gmail.com>
2025-12-30 22:51:02 +0100

    fix(zsh): fix dirstack navigation error

--- zsh/.config/zsh/utils/dirstack/write-dir-history.zsh
+++ zsh/.config/zsh/utils/dirstack/write-dir-history.zsh
@@ -5,5 +5,9 @@ local file=$ZSH_CACHE_DIR/dir-history-$EUID
 local tmp=$file.tmp.$$
 
 {
-  print -rC1 -- $_zsh_dir_history >$tmp && mv -f -- $tmp $file
+  if print -rC1 -- $_zsh_dir_history >$tmp 2>/dev/null; then
+    mv -f -- $tmp $file 2>/dev/null
+  else
+    rm -f -- $tmp 2>/dev/null
+  fi
 } &!
