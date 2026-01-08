bdba21999ddff3c90452f9fc8204e4984e5948aa
Damian D'Souza <dsouzadamian189@gmail.com>
2026-01-08 14:57:42 +0100

    chore(zsh): add jk binds to fzf-git

--- zsh/.config/zsh/utils/plugins-config/fzf-git.zsh
+++ zsh/.config/zsh/utils/plugins-config/fzf-git.zsh
@@ -8,6 +8,11 @@ else
       --border-label-pos 2 \
       --color 'label:blue' \
       --preview-window 'right,50%' --preview-border line \
+      --bind 'j:down' \
+      --bind 'k:up' \
+      --bind 'load:disable-search' \
+      --bind 'i:enable-search+unbind(j)+unbind(k)+unbind(i)' \
+      --bind 'esc:disable-search+rebind(j)+rebind(k)+rebind(i)' \
       --bind 'ctrl-/:change-preview-window(down,50%|hidden|)' "$@"
   }
 fi
