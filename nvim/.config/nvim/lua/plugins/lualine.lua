787d5cfc9b2f560bafb8ec1e05c6d791824cc52c
Damian D'Souza <dsouzadamian189@gmail.com>
2026-01-08 14:57:23 +0100

    chore(nvim): add branch name length limit in lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -7,6 +7,18 @@ return {
     },
     sections = {
       lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+      lualine_b = {
+        {
+          "branch",
+          fmt = function(str)
+            if vim.api.nvim_strwidth(str) > 40 then
+              return ("%s…"):format(str:sub(1, 39))
+            end
+
+            return str
+          end,
+        },
+      },
       lualine_z = {
         {
           function()
