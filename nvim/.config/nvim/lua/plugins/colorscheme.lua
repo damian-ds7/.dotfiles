cc35a5898a5b6df3f12aba88ea6dcb546d78923b
Damian D'Souza <dsouzadamian189@gmail.com>
2026-01-18 08:44:12 +0100

    feat(nvim): add floating style for lualine and bufferline

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,25 @@
 return {
   {
-    "LazyVim/Lazyvim",
+    "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
     },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = function(_, opts)
+      opts = opts or {}
+      opts.transparent_background = true
+      -- opts.float = {
+      --   transparent = true,
+      --   solid = false,
+      -- }
+      -- opts.custom_highlights = {
+      --   SnackNormal = { link = "NormalFloat" },
+      --   SnacksNormalNC = { link = "NormalFloat" },
+      -- }
+      return opts
+    end,
   },
 }
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,7 @@ return {
   "nvim-lualine/lualine.nvim",
   opts = {
     options = {
+      theme = "auto",
       component_separators = "",
       section_separators = { left = "", right = "" },
     },
