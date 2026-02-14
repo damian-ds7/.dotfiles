50f569d8de8cc58edbb8f31e4131158fb6657ee9
Damian D'Souza <dsouzadamian189@gmail.com>
2026-02-14 19:11:53 +0100

    feat(nvim): add function to create socket on launch

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,26 @@
+local socket_dir = os.getenv("NVIM_SOCKET_DIR")
+
+if socket_dir then
+  if vim.fn.isdirectory(socket_dir) == 0 then
+    vim.fn.mkdir(socket_dir, "p")
+  end
+
+  local seconds = os.time()
+  local nsec = vim.uv.hrtime() % 1000000000
+  local socket_name = string.format("nvim-%d%09d.sock", seconds, nsec)
+  local socket_path = socket_dir .. "/" .. socket_name
+
+  local ok, err = pcall(vim.fn.serverstart, socket_path)
+
+  if ok then
+    vim.api.nvim_create_autocmd("VimLeavePre", {
+      callback = function()
+        vim.fn.delete(socket_path)
+      end,
+    })
+  end
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
