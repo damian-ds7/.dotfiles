275aec4cc5a1dfd777dd12bf8534c36b011b77d9
Damian D'Souza <dsouzadamian189@gmail.com>
2026-02-24 16:40:01 +0100

    fix(nvim): make tmux navigator work in terminal mode

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,18 +1,28 @@
 return {
   "christoomey/vim-tmux-navigator",
+  lazy = false,
   cmd = {
     "TmuxNavigateLeft",
     "TmuxNavigateDown",
     "TmuxNavigateUp",
     "TmuxNavigateRight",
     "TmuxNavigatePrevious",
-    "TmuxNavigatorProcessList",
   },
-  keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-  },
+  config = function()
+    local modes = { "n", "v", "t" }
+    local mappings = {
+      ["<C-h>"] = "TmuxNavigateLeft",
+      ["<C-j>"] = "TmuxNavigateDown",
+      ["<C-k>"] = "TmuxNavigateUp",
+      ["<C-l>"] = "TmuxNavigateRight",
+      ["<C-\\>"] = "TmuxNavigatePrevious",
+    }
+
+    for key, command in pairs(mappings) do
+      vim.keymap.set(modes, key, "<cmd>" .. command .. "<cr>", {
+        desc = "Tmux Navigation: " .. command,
+        silent = true,
+      })
+    end
+  end,
 }
