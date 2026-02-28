8b24163a28bc90bb2832f5db66083e7c4113cb65
Damian D'Souza <dsouzadamian189@gmail.com>
2026-02-28 19:03:40 +0100

    chore(nvim): remove nvim socket dir

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,24 +1,6 @@
 require "config.options"
 require "config.keymaps"
 require "config.autocmds"
-
-local socket_dir = os.getenv "NVIM_SOCKET_DIR"
-
-if socket_dir then
-  if vim.fn.isdirectory(socket_dir) == 0 then vim.fn.mkdir(socket_dir, "p") end
-
-  local seconds = os.time()
-  local nsec = vim.uv.hrtime() % 1000000000
-  local socket_name = string.format("nvim-%d%09d.sock", seconds, nsec)
-  local socket_path = socket_dir .. "/" .. socket_name
-
-  local ok, _ = pcall(vim.fn.serverstart, socket_path)
-
-  if ok then vim.api.nvim_create_autocmd("VimLeavePre", {
-    callback = function() vim.fn.delete(socket_path) end,
-  }) end
-end
-
 require "config.lazy"
 require "config.lsp"
 
--- nvim/.config/nvim/themes/switch-theme.sh
+++ nvim/.config/nvim/themes/switch-theme.sh
@@ -5,18 +5,16 @@ SCHEME="${1?Error: SCHEME (argument 1) is required}"
 MODE="${2?Error: MODE (argument 2) is required}"
 
 (
-  cd "$HOME"/.config/nvim/themes || exit
-  ln -sfr "$SCHEME/$MODE" current-theme
+    cd "$HOME"/.config/nvim/themes || exit
+    ln -sfr "$SCHEME/$MODE" current-theme
 
-  scheme
-  read -r scheme <current-theme
+    scheme
+    read -r scheme <current-theme
 
-  if [[ -d "$NVIM_SOCKET_DIR" ]]; then
-    for sock in "$NVIM_SOCKET_DIR"/*; do
-      if [[ -S "$sock" ]]; then
-        nvim --server "$sock" --remote-send ":silent colorscheme $scheme<CR>"
-        echo "NVIM: theme '$scheme' set for socket '$sock'" >&1
-      fi
+    for sock in "${XDG_RUNTIME_DIR:-/run/user/$UID}"/nvim.*; do
+        if [[ -S "$sock" ]]; then
+            nvim --server "$sock" --remote-send ":silent colorscheme $scheme<CR>"
+            echo "NVIM: theme '$scheme' set for socket '$sock'" >&1
+        fi
     done
-  fi
 )
