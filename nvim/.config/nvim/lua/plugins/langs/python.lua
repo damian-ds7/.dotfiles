c52827542720b4f016d84eb34fd75b49ba4d37ef
Damian D'Souza <dsouzadamian189@gmail.com>
2026-03-15 12:37:03 +0100

    feat(nvim): replace pyright with pyrefly

--- nvim/.config/nvim/lua/plugins/langs/python.lua
+++ nvim/.config/nvim/lua/plugins/langs/python.lua
@@ -5,7 +5,20 @@ local lang = {
     ruff = {
       on_attach = function(client) client.server_capabilities.hoverProvider = false end,
     },
-    pyright = {},
+    pyrefly = {
+      on_attach = function(client, bufnr)
+        if client:supports_method "textDocument/inlayHint" then
+          vim.defer_fn(function() vim.lsp.inlay_hint.enable(true, { bufnr = bufnr }) end, 100)
+        end
+      end,
+      settings = {
+        python = {
+          pyrefly = {
+            displayTypeErrors = "force-on",
+          },
+        },
+      },
+    },
   },
 
   tools = { "debugpy" },
