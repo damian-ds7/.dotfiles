36b523ec88ea23810569d0e9387badc9d69cc307
Damian D'Souza <dsouzadamian189@gmail.com>
2026-03-17 14:19:50 +0100

    chore(niri): fic vicinae battery threshold script

--- niri/.local/share/vicinae/scripts/battery-threshold.sh
+++ niri/.local/share/vicinae/scripts/battery-threshold.sh
@@ -8,19 +8,19 @@
 # @vicinae.keywords ["battery", "threshold", "power"]
 
 if [ -z "${1-}" ]; then
-  qs ipc call plugin:battery-threshold togglePanel
-  exit 0
+    qs -c Noctalia ipc call plugin:battery-threshold togglePanel
+    exit 0
 fi
 
 if [ "$#" -eq 1 ]; then
-  if [[ "$1" =~ ^[0-9]+$ ]]; then
-    qs ipc call plugin:battery-threshold set "$1"
-    echo "Threshold set to $1"
-    exit 0
-  fi
+    if [[ "$1" =~ ^[0-9]+$ ]]; then
+        qs -c Noctalia ipc call plugin:battery-threshold set "$1"
+        echo "Threshold set to $1"
+        exit 0
+    fi
 
-  echo "Threshold must be a number." >&2
-  exit 1
+    echo "Threshold must be a number." >&2
+    exit 1
 fi
 
 echo "Usage: battery-threshold [number]" >&2
