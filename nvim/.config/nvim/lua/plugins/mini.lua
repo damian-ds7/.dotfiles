3fd9883beb4912d85112df4832a4b7f8969377db
Damian D'Souza <dsouzadamian189@gmail.com>
2026-03-19 16:02:15 +0100

    feat(nvim): add <> to mini.pairs

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -61,6 +61,21 @@ return {
       skip_ts = { "string" },
       skip_unbalanced = true,
       markdown = true,
+      mappings = {
+        ["("] = { action = "open", pair = "()", neigh_pattern = "^[^\\]" },
+        ["["] = { action = "open", pair = "[]", neigh_pattern = "^[^\\]" },
+        ["{"] = { action = "open", pair = "{}", neigh_pattern = "^[^\\]" },
+        ["<"] = { action = "open", pair = "<>", neigh_pattern = "^[^\\]" },
+
+        [")"] = { action = "close", pair = "()", neigh_pattern = "^[^\\]" },
+        ["]"] = { action = "close", pair = "[]", neigh_pattern = "^[^\\]" },
+        ["}"] = { action = "close", pair = "{}", neigh_pattern = "^[^\\]" },
+        [">"] = { action = "close", pair = "<>", neigh_pattern = "^[^\\]" },
+
+        ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "^[^\\]", register = { cr = false } },
+        ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "^[^%a\\]", register = { cr = false } },
+        ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "^[^\\]", register = { cr = false } },
+      },
     },
   },
   {
