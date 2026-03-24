09ad63fdccdcd29f3e10796da1fc964b9dc25595
Damian D'Souza <dsouzadamian189@gmail.com>
2026-03-24 20:51:59 +0100

    chore: replace keyd caps config with kanata

--- keyd/.config/keyd/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-sudo mkdir -p /etc/keyd/
-sudo ln -sf "$SCRIPT_DIR/default.conf" /etc/keyd/default.conf
-sudo systemctl restart keyd
