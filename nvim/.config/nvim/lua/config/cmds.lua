01a0ace04652eb32cc336b5942e5b9793fdc0343
Damian D'Souza <dsouzadamian189@gmail.com>
2026-04-03 12:31:10 +0200

    feat(nvim): add lsp commands

--- nvim/.config/nvim/lua/config/cmds.lua
+++ nvim/.config/nvim/lua/config/cmds.lua
@@ -6,3 +6,20 @@ vim.api.nvim_create_user_command("PackDel", function(opts)
   local names = vim.split(opts.args, ",", { trimws = true })
   vim.pack.del(names)
 end, { nargs = "+" })
+
+vim.api.nvim_create_user_command("LspInfo", "checkhealth vim.lsp", {
+  desc = "Show LSP Info",
+})
+
+vim.api.nvim_create_user_command("LspLog", function(_)
+  local state_path = vim.fn.stdpath "state"
+  local log_path = vim.fs.joinpath(state_path, "lsp.log")
+
+  vim.cmd(string.format("edit %s", log_path))
+end, {
+  desc = "Show LSP log",
+})
+
+vim.api.nvim_create_user_command("LspRestart", "lsp restart", {
+  desc = "Restart LSP",
+})
