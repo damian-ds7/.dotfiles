add26818c9e5977e8c45e0ca9dbb943151cfb774
Damian D'Souza <dsouzadamian189@gmail.com>
2026-04-15 00:33:13 +0200

    feat(nvim): add bind to open custom location in oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -50,9 +50,48 @@ utils.add(utils.gh "stevearc/oil.nvim", function()
   }
 end)
 
-vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
-vim.keymap.set("n", "<leader>e", function() require("oil").toggle_float() end, { desc = "Open parent directory" })
+vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "(Oil) Open parent directory" })
+vim.keymap.set("n", "<leader>e", function() require("oil").toggle_float() end, { desc = "(Oil) Open parent directory" })
 vim.keymap.set("n", "<leader>E", function()
   local root = vim.lsp.buf.list_workspace_folders()[1] or vim.fn.getcwd()
   require("oil").toggle_float(root)
-end, { desc = "Open Project Root" })
+end, { desc = "(Oil) Open Project Root" })
+
+local last_oil_path = nil
+
+local function open_oil_float(path)
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    if vim.bo[buf].filetype == "oil" then
+      local cfg = vim.api.nvim_win_get_config(win)
+      if cfg.relative ~= "" then
+        vim.api.nvim_set_current_win(win)
+        vim.cmd("Oil " .. vim.fn.fnameescape(path))
+        return
+      end
+    end
+  end
+
+  vim.cmd("Oil --float " .. vim.fn.fnameescape(path))
+end
+
+local function prompt_and_open()
+  vim.ui.input({
+    prompt = "Oil path: ",
+    completion = "dir",
+  }, function(input)
+    local target = (input and input ~= "") and input or vim.fn.getcwd()
+    last_oil_path = target
+    open_oil_float(target)
+  end)
+end
+
+vim.keymap.set("n", "<leader>o", prompt_and_open, { desc = "(Oil) Open location" })
+
+vim.keymap.set("n", "<leader>O", function()
+  if last_oil_path then
+    open_oil_float(last_oil_path)
+  else
+    prompt_and_open()
+  end
+end, { desc = "(Oil) Reopen last" })
