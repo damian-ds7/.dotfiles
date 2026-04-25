16ca09c0a1758e490307513a9a55db10e414dd99
Damian D'Souza <dsouzadamian189@gmail.com>
2026-04-25 14:17:32 +0200

    refactor(nvim): move funcs out of lsp autocmds, add desc and group

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,63 +1,79 @@
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("user-lsp-attach", { clear = true }),
+local icons = require "utils.icons"
+local builtin = lazy.require_on_index "telescope.builtin"
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local lsp_attach_group = augroup("lsp-attach", { clear = true })
+local lsp_highlight_group = augroup("lsp-highlight", { clear = false })
+local lsp_inlay_group = augroup("lsp-inlay-hints", { clear = false })
+local lsp_diagnostics_group = augroup("lsp-diagnostics", { clear = false })
+
+local function map(buf, keys, func, desc, mode)
+  mode = mode or "n"
+  vim.keymap.set(mode, keys, func, {
+    buffer = buf,
+    desc = "LSP: " .. desc,
+  })
+end
+
+autocmd("LspAttach", {
+  group = lsp_attach_group,
   callback = function(event)
     local client = vim.lsp.get_client_by_id(event.data.client_id)
     local buf = event.buf
 
-    local map = function(keys, func, desc, mode)
-      mode = mode or "n"
-      vim.keymap.set(mode, keys, func, { buffer = buf, desc = "LSP: " .. desc })
-    end
+    if not client then return end
 
-    if client and client.server_capabilities.documentHighlightProvider then
-      local highlight_group = vim.api.nvim_create_augroup("user-lsp-highlight", { clear = false })
+    map(buf, "<leader>cr", vim.lsp.buf.rename, "Rename")
+    map(buf, "<leader>ca", vim.lsp.buf.code_action, "Code Action", { "n", "x" })
+    map(buf, "<leader>cl", vim.lsp.codelens.run, "Codelens", { "n", "x" })
+    map(buf, "<leader>cc", vim.lsp.buf.declaration, "Goto Declaration")
 
-      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+    map(buf, "<leader>cD", function() builtin.lsp_definitions { reuse_win = true } end, "Definition")
+    map(buf, "<leader>cR", function() builtin.lsp_references { reuse_win = true } end, "References")
+    map(buf, "<leader>ci", function() builtin.lsp_implementations { reuse_win = true } end, "Implementation")
+    map(buf, "<leader>ct", function() builtin.lsp_type_definitions { reuse_win = true } end, "Type Definition")
+    map(buf, "<leader>cs", function() builtin.lsp_document_symbols() end, "Document Symbols")
+    map(buf, "<leader>cS", function() builtin.lsp_dynamic_workspace_symbols() end, "Workspace Symbols")
+
+    map(buf, "grD", vim.lsp.buf.declaration, "Goto Declaration")
+    map(buf, "gD", vim.lsp.buf.declaration, "Goto Declaration")
+    map(buf, "grr", function() builtin.lsp_references { reuse_win = true } end, "References")
+    map(buf, "gd", function() builtin.lsp_definitions { reuse_win = true } end, "Definition")
+    map(buf, "gri", function() builtin.lsp_implementations { reuse_win = true } end, "Implementation")
+    map(buf, "grt", function() builtin.lsp_type_definitions { reuse_win = true } end, "Type Definition")
+    map(buf, "gs", function() builtin.lsp_document_symbols() end, "Document Symbols")
+    map(buf, "gS", function() builtin.lsp_dynamic_workspace_symbols() end, "Workspace Symbols")
+
+    if client.server_capabilities.documentHighlightProvider then
+      autocmd({ "CursorHold", "CursorHoldI" }, {
+        group = lsp_highlight_group,
         buffer = buf,
-        group = highlight_group,
         callback = vim.lsp.buf.document_highlight,
       })
 
-      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+      autocmd({ "CursorMoved", "CursorMovedI" }, {
+        group = lsp_highlight_group,
         buffer = buf,
-        group = highlight_group,
         callback = vim.lsp.buf.clear_references,
       })
     end
-    local builtin = lazy.require_on_index "telescope.builtin"
 
-    map("<leader>cr", vim.lsp.buf.rename, "Rename")
-    map("<leader>ca", vim.lsp.buf.code_action, "Code Action", { "n", "x" })
-    map("<leader>cl", vim.lsp.codelens.run, "Codelens", { "n", "x" })
-    map("<leader>cc", vim.lsp.buf.declaration, "Goto Declaration")
-    map("<leader>cD", function() builtin.lsp_definitions { reuse_win = true } end, "Definition")
-    map("<leader>cR", function() builtin.lsp_references { reuse_win = true } end, "References")
-    map("<leader>ci", function() builtin.lsp_implementations { reuse_win = true } end, "Implementation")
-    map("<leader>ct", function() builtin.lsp_type_definitions { reuse_win = true } end, "Type Definition")
-    map("<leader>cs", function() builtin.lsp_document_symbols() end, "Document Symbols")
-    map("<leader>cS", function() builtin.lsp_dynamic_workspace_symbols() end, "Workspace Symbols")
+    if client:supports_method("textDocument/inlayHint", buf) then
+      vim.lsp.inlay_hint.enable(true, { bufnr = buf })
 
-    map("grD", vim.lsp.buf.declaration, "Goto Declaration")
-    map("gD", vim.lsp.buf.declaration, "Goto Declaration")
-    map("grr", function() builtin.lsp_references { reuse_win = true } end, "References")
-    map("gd", function() builtin.lsp_definitions { reuse_win = true } end, "Definition")
-    map("gri", function() builtin.lsp_implementations { reuse_win = true } end, "Implementation")
-    map("grt", function() builtin.lsp_type_definitions { reuse_win = true } end, "Type Definition")
-    map("gs", function() builtin.lsp_document_symbols() end, "Document Symbols")
-    map("gS", function() builtin.lsp_dynamic_workspace_symbols() end, "Workspace Symbols")
-
-    if client and client:supports_method("textDocument/inlayHint", buf) then
-      vim.lsp.inlay_hint.enable(true)
-      vim.keymap.set(
-        "n",
+      map(
+        buf,
         "<leader>uh",
-        function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = buf }) end,
-        { buffer = buf, desc = "Toggle Inlay Hints" }
+        function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = buf }, { bufnr = buf }) end,
+        "Toggle Inlay Hints"
       )
 
       local hint_state = false
 
-      vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+      autocmd({ "InsertEnter", "InsertLeave" }, {
+        group = lsp_inlay_group,
         buffer = buf,
         callback = function(args)
           if args.event == "InsertEnter" then
@@ -70,22 +86,23 @@ vim.api.nvim_create_autocmd("LspAttach", {
       })
     end
 
-    if client and client:supports_method "textDocument/codeLens" then
+    if client:supports_method "textDocument/codeLens" then
       vim.lsp.codelens.enable(true)
-      vim.keymap.set(
-        "n",
+
+      map(
+        buf,
         "<leader>ue",
         function() vim.lsp.codelens.enable(not vim.lsp.codelens.is_enabled { bufnr = buf }) end,
-        { buffer = buf, desc = "Toggle Codelens" }
+        "Toggle Codelens"
       )
     end
   end,
 })
 
 vim.api.nvim_create_autocmd("VimEnter", {
+  group = lsp_diagnostics_group,
   once = true,
   callback = function()
-    local icons = require "utils.icons"
     vim.diagnostic.config {
       underline = true,
       update_in_insert = false,
@@ -103,7 +120,10 @@ vim.api.nvim_create_autocmd("VimEnter", {
           [vim.diagnostic.severity.INFO] = icons.diagnostics.Info,
         },
       },
-      float = { border = "rounded", source = "if_many" },
+      float = {
+        border = "rounded",
+        source = "if_many",
+      },
     }
   end,
 })
