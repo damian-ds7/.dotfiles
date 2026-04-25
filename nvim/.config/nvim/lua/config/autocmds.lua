1b88ee703f68d5a04b2a9c8d5cfd08142f09c13a
Damian D'Souza <dsouzadamian189@gmail.com>
2026-04-25 13:58:32 +0200

    feat(nvim): add autocmd to remove trailing whitespace chars

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -3,7 +3,7 @@ local augroup = vim.api.nvim_create_augroup
 
 autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
-  group = augroup("kickstart-highlight-yank", { clear = true }),
+  group = augroup("highlight-yank", { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
 
@@ -11,4 +11,15 @@ autocmd("ColorScheme", {
   desc = "Set inlay hints color",
   group = augroup("inlay-hints-bg", { clear = true }),
   callback = function() vim.api.nvim_set_hl(0, "LspInlayHint", { link = "Comment" }) end,
+})
+
+autocmd("BufWritePre", {
+  desc = "Remove trailing whitespaces on save",
+  group = augroup("trim-whitespace", { clear = true }),
+  pattern = "*",
+  callback = function()
+    local view = vim.fn.winsaveview()
+    vim.cmd [[%s/\s\+$//e]]
+    vim.fn.winrestview(view)
+  end,
 })
