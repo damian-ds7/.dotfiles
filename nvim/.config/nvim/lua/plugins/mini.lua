783fc24d737f97535aacf3266660f53e084b9419
Damian D'Souza <dsouzadamian189@gmail.com>
2026-05-12 18:47:50 +0200

    refactor(nvim): move session logic to separate file

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -125,64 +125,12 @@ utils.add(utils.gh "nvim-mini/mini.icons", function()
 end)
 
 utils.add(utils.gh "nvim-mini/mini.sessions", function()
-  local sessions = require "mini.sessions"
-
-  local session_loaded = false
-
-  sessions.setup {
+  require("mini.sessions").setup {
     force = {
       read = false,
       write = true,
       delete = true,
     },
   }
-
-  local function encode(path)
-    return path:gsub("/", "-"):gsub("%-+", "-"):gsub("^%-", ""):gsub("%-$", "")
-  end
-
-  local function current_session_name() return encode(vim.fn.getcwd()) end
-
-  local function restore_session()
-    local name = current_session_name()
-    local detected = sessions.detected or {}
-    if detected[name] then
-      session_loaded = true
-      sessions.read(name)
-    end
-  end
-
-  vim.keymap.set("n", "<leader>qs", restore_session, {
-    desc = "Restore Session",
-  })
-
-  vim.keymap.set("n", "<leader>ql", function() sessions.select() end, {
-    desc = "Select Session",
-  })
-
-  vim.keymap.set("n", "<leader>qw", function() sessions.write() end, {
-    desc = "Save Session",
-  })
-
-  vim.keymap.set("n", "<leader>qd", function() sessions.delete() end, {
-    desc = "Delete Session",
-  })
-
-  vim.keymap.set(
-    "n",
-    "<leader>R",
-    function() sessions.restart() end,
-    { desc = "Restart Neovim" }
-  )
-
-  vim.api.nvim_create_autocmd("BufReadPost", {
-    group = vim.api.nvim_create_augroup("session-auto-save", { clear = true }),
-    once = true,
-    callback = function()
-      if vim.v.this_session ~= "" then return end
-      if session_loaded then return end
-      if vim.bo.buftype ~= "" or vim.fn.expand "%" == "" then return end
-      sessions.write(current_session_name(), { verbose = false })
-    end,
-  })
+  require("utils.session").setup()
 end)
