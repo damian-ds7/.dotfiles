877479baa8c07073d69305798030483d3f643a51
Damian D'Souza <dsouzadamian189@gmail.com>
2026-05-14 12:26:08 +0200

    feat(nvim): add lazydev plugin

--- nvim/.config/nvim/lua/plugins/langs/lua.lua
+++ nvim/.config/nvim/lua/plugins/langs/lua.lua
@@ -1,58 +1,40 @@
-return lang {
-  servers = {
-    lua_ls = {
-      mason_name = "lua-language-server",
-      on_attach = function(_, buf)
-        -- HACK: enabling inlay hints won't work in LspAttach, needs to be delayed
-        vim.defer_fn(
-          function() vim.lsp.inlay_hint.enable(true, { bufnr = buf }) end,
-          100
-        )
-      end,
-      on_init = function(client)
-        if client.workspace_folders then
-          local path = client.workspace_folders[1].name
-          if
-            path ~= vim.fn.stdpath "config"
-            and (
-              vim.uv.fs_stat(path .. "/.luarc.json")
-              or vim.uv.fs_stat(path .. "/.luarc.jsonc")
-            )
-          then
-            return
-          end
-        end
-
-        client.config.settings.Lua =
-          vim.tbl_deep_extend("force", client.config.settings.Lua, {
-            runtime = {
-              version = "LuaJIT",
-              path = { "lua/?.lua", "lua/?/init.lua" },
+return {
+  lang {
+    servers = {
+      lua_ls = {
+        mason_name = "lua-language-server",
+        on_attach = function(_, buf)
+          -- HACK: enabling inlay hints won't work in LspAttach, needs to be delayed
+          vim.defer_fn(
+            function() vim.lsp.inlay_hint.enable(true, { bufnr = buf }) end,
+            100
+          )
+        end,
+        ---@type lspconfig.settings.lua_ls
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = "Replace",
             },
-            diagnostics = { globals = { "vim", "Snacks" } },
-            workspace = {
-              checkThirdParty = false,
-              library = {
-                vim.env.VIMRUNTIME,
-                -- Depending on the usage, you might want to add additional paths
-                -- here.
-                -- '${3rd}/luv/library',
-                -- '${3rd}/busted/library',
-              },
+            diagnostics = {
+              globals = { "vim", "Snacks" },
             },
-          })
-      end,
-      settings = {
-        Lua = {
-          completion = {
-            callSnippet = "Replace",
           },
         },
       },
     },
+
+    tools = { "stylua" },
+
+    treesitter = { "lua", "luadoc", "luap" },
   },
-
-  tools = { "stylua" },
-
-  treesitter = { "lua", "luadoc", "luap" },
+  plugin {
+    src = "folke/lazydev.nvim",
+    filetype = "lua",
+    opts = {
+      library = {
+        { path = "nvim-lspconfig", words = { "lspconfig", "lsp", "servers" } },
+      },
+    },
+  },
 }
