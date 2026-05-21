603dff3441fa44b34a4f1b26b9a5906ed9327b13
Damian D'Souza <dsouzadamian189@gmail.com>
2026-05-21 13:32:29 +0200

    chore(nvim): set show_hidden to false in oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -57,7 +57,7 @@ return plugin {
     delete_to_trash = true,
     skip_confirm_for_simple_edits = true,
     view_options = {
-      show_hidden = true,
+      show_hidden = false,
       natural_order = true,
       is_always_hidden = function(name, _) return name == ".." or name == ".git" end,
     },
