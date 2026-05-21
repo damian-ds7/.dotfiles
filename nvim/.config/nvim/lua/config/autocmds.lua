c8edb07974d100b6feb99eef9943b53cd1db10c1
Damian D'Souza <dsouzadamian189@gmail.com>
2026-05-21 13:45:17 +0200

    chore(nvim): update highligh-yank autocmd

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -4,7 +4,14 @@ local augroup = vim.api.nvim_create_augroup
 autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = augroup("highlight-yank", { clear = true }),
-  callback = function() vim.hl.on_yank() end,
+  callback = function()
+    if vim.version().minor < 13 then
+      -- TODO: remove
+      vim.hl.on_yank()
+    else
+      vim.hl.hl_op()
+    end
+  end,
 })
 
 autocmd("ColorScheme", {
