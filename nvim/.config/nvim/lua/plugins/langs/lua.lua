2687a9affc742d093cf68835f9101b18e02c2c39
Damian D'Souza <dsouzadamian189@gmail.com>
2026-06-04 18:50:17 +0200

    chore(nvim): remove layzdev

--- nvim/.config/nvim/lua/plugins/langs/lua.lua
+++ nvim/.config/nvim/lua/plugins/langs/lua.lua
@@ -1,40 +1,37 @@
-return {
-  lang {
-    servers = {
-      lua_ls = {
-        mason_name = "lua-language-server",
-        on_attach = function(_, buf)
-          -- HACK: enabling inlay hints won't work in LspAttach, needs to be delayed
-          vim.defer_fn(
-            function() vim.lsp.inlay_hint.enable(true, { bufnr = buf }) end,
-            100
-          )
-        end,
-        ---@type lspconfig.settings.lua_ls
-        settings = {
-          Lua = {
-            completion = {
-              callSnippet = "Replace",
-            },
-            diagnostics = {
-              globals = { "vim", "Snacks" },
-            },
+return lang {
+  servers = {
+    lua_ls = {
+      mason_name = "lua-language-server",
+      on_attach = function(_, buf)
+        -- HACK: enabling inlay hints won't work in LspAttach, needs to be delayed
+        vim.defer_fn(
+          function() vim.lsp.inlay_hint.enable(true, { bufnr = buf }) end,
+          100
+        )
+      end,
+      ---@type lspconfig.settings.lua_ls
+      settings = {
+        Lua = {
+          runtime = {
+            version = "LuaJIT",
+            path = vim.split(package.path, ";"),
+          },
+          diagnostics = {
+            globals = { "vim", "Snacks" },
+          },
+          workspace = {
+            library = { vim.env.VIMRUNTIME },
+            checkThirdParty = false,
+          },
+          telemetry = {
+            enable = false,
           },
         },
       },
     },
-
-    tools = { "stylua" },
-
-    treesitter = { "lua", "luadoc", "luap" },
   },
-  plugin {
-    src = "folke/lazydev.nvim",
-    filetype = "lua",
-    opts = {
-      library = {
-        { path = "nvim-lspconfig", words = { "lspconfig", "lsp", "servers" } },
-      },
-    },
-  },
+
+  tools = { "stylua" },
+
+  treesitter = { "lua", "luadoc", "luap" },
 }
