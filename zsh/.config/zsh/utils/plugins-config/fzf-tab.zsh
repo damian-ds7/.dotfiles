9dceecd978e213c0525a0dd5465b1f2bdaf00d86
Damian D'Souza <dsouzadamian189@gmail.com>
2026-07-08 16:09:12 +0200

    refactor(zsh): use ls for preview in fzf if lsd not available

--- zsh/.config/zsh/utils/plugins-config/fzf-tab.zsh
+++ zsh/.config/zsh/utils/plugins-config/fzf-tab.zsh
@@ -12,7 +12,14 @@ zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ':fzf-tab:*' fzf-bindings 'tab:accept' 'enter:accept' 'ctrl-y:accept'
 zstyle ':fzf-tab:*' accept-line enter
 zstyle ':fzf-tab:*' continuous-trigger '/'
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'lsd -1 --color=always --icon=always -- $realpath'
+
+if command -v lsd >/dev/null 2>&1; then
+  LS_COMMAND="lsd -1 --color=always --icon=always"
+else
+  LS_COMMAND="ls --color=always -1"
+fi
+
+zstyle ':fzf-tab:complete:cd:*' fzf-preview "$LS_COMMAND -- \$realpath"
 zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview \
   'SYSTEMD_COLORS=1 systemctl status $word 2>/dev/null || SYSTEMD_COLORS=1 systemctl --user status $word 2>/dev/null'
 zstyle ':fzf-tab:*' fzf-flags --ansi
