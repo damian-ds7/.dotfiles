7cf2f9b3799d28bfcced3154d1ac893e7ffe9d5f
Damian D'Souza <dsouzadamian189@gmail.com>
2026-07-16 11:17:22 +0200

    feat(zsh): add zsh-system-clipboard plugin

--- zsh/.config/zsh/utils/init-plugins.zsh
+++ zsh/.config/zsh/utils/init-plugins.zsh
@@ -4,28 +4,29 @@ SKIP_PLUGINS_FILE="${ZSH_CACHE_DIR:-$HOME/.cache}/zsh/skip-plugins"
 [[ -f "$SKIP_PLUGINS_FILE" ]] && return 0
 
 function zcompile-many() {
-	local f
-	setopt localoptions nullglob
-	for f in "$@"; do
-		if [[ -f "$f" ]]; then
-			if [[ ! -f "${f}.zwc" || "$f" -nt "${f}.zwc" ]]; then
-				zcompile "${f}.zwc" "$f" 2>/dev/null || true
-			fi
-		fi
-	done
+  local f
+  setopt localoptions nullglob
+  for f in "$@"; do
+    if [[ -f "$f" ]]; then
+      if [[ ! -f "${f}.zwc" || "$f" -nt "${f}.zwc" ]]; then
+        zcompile "${f}.zwc" "$f" 2>/dev/null || true
+      fi
+    fi
+  done
 }
 
 zcompile-many "$ZDOTDIR"/plugins/syntax-highlighting/zsh-syntax-highlighting.zsh \
-	"$ZDOTDIR"/plugins/syntax-highlighting/highlighters/*/*.zsh
+  "$ZDOTDIR"/plugins/syntax-highlighting/highlighters/*/*.zsh
 
 zcompile-many "$ZDOTDIR"/plugins/autosuggestions/zsh-autosuggestions.zsh \
-	"$ZDOTDIR"/plugins/autosuggestions/src/**/*.zsh
+  "$ZDOTDIR"/plugins/autosuggestions/src/**/*.zsh
 
 zcompile-many "$ZDOTDIR/plugins/powerlevel10k/powerlevel10k.zsh-theme" \
-	"$ZDOTDIR/plugins/ssh-agent/ssh-agent.zsh" \
-	"$ZDOTDIR/utils/setup-vimode.zsh" \
-	"$ZDOTDIR/plugins/fzf-tab/fzf-tab.zsh" \
-	"$ZDOTDIR/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh"
+  "$ZDOTDIR/plugins/ssh-agent/ssh-agent.zsh" \
+  "$ZDOTDIR/utils/setup-vimode.zsh" \
+  "$ZDOTDIR/plugins/fzf-tab/fzf-tab.zsh" \
+  "$ZDOTDIR/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh"
+"$ZDOTDIR/plugins/zsh-system-clipboard/zsh-system-clipboard.zsh"
 
 autoload -Uz "$ZDOTDIR/plugins/defer/zsh-defer"
 
@@ -52,6 +53,9 @@ source "$ZDOTDIR/utils/plugins-config/fzf-tab.zsh"
 source "$ZDOTDIR/plugins/fzf-git/fzf-git.sh"
 source "$ZDOTDIR/utils/plugins-config/fzf-git.zsh"
 
+source "$ZDOTDIR/plugins/zsh-system-clipboard/zsh-system-clipboard.zsh"
+source "$ZDOTDIR/utils/plugins-config/zsh-system-clipboard.zsh"
+
 source "$ZDOTDIR/utils/plugins-config/zsh-autosuggestions.zsh"
 zsh-defer source "$ZDOTDIR/plugins/autosuggestions/zsh-autosuggestions.zsh"
 
@@ -61,16 +65,16 @@ source "$ZDOTDIR/utils/plugins-config/zsh-history-substring-search.zsh"
 source "$ZDOTDIR/plugins/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh"
 
 if [[ -d "$ZDOTDIR/utils/dirstack" ]]; then
-	zcompile-many "$ZDOTDIR/utils/dirstack/**/*.zsh" true
+  zcompile-many "$ZDOTDIR/utils/dirstack/**/*.zsh" true
 
-	source "$ZDOTDIR/utils/dirstack/nav.zsh"
-	source "$ZDOTDIR/utils/dirstack/read-dir-history.zsh"
-	source "$ZDOTDIR/utils/dirstack/update-dir-history.zsh"
-	source "$ZDOTDIR/utils/dirstack/write-dir-history.zsh"
+  source "$ZDOTDIR/utils/dirstack/nav.zsh"
+  source "$ZDOTDIR/utils/dirstack/read-dir-history.zsh"
+  source "$ZDOTDIR/utils/dirstack/update-dir-history.zsh"
+  source "$ZDOTDIR/utils/dirstack/write-dir-history.zsh"
 
-	if typeset -f -- _zsh_update_dir_history >/dev/null; then
-		autoload -Uz add-zsh-hook
-		add-zsh-hook chpwd _zsh_update_dir_history
-		_zsh_update_dir_history
-	fi
+  if typeset -f -- _zsh_update_dir_history >/dev/null; then
+    autoload -Uz add-zsh-hook
+    add-zsh-hook chpwd _zsh_update_dir_history
+    _zsh_update_dir_history
+  fi
 fi
