f2d5e9b0a176ee817f049dcdedd7ba0a6ea8f466
Damian D'Souza <dsouzadamian189@gmail.com>
2026-09-04 10:30:33 +0200

    chore(nvim): remove redundant pack commands

--- nvim/.config/nvim/lua/config/cmds.lua
+++ nvim/.config/nvim/lua/config/cmds.lua
@@ -10,37 +10,6 @@ vim.api.nvim_create_user_command(
   {}
 )
 
-vim.api.nvim_create_user_command("PackUpdate", function(opts)
-  local names, pack_opts = {}, { force = opts.bang }
-  for _, arg in ipairs(opts.fargs) do
-    if arg == "--offline" then
-      pack_opts.offline = true
-    elseif arg:match "^--target=" then
-      pack_opts.target = arg:match "^--target=(.*)"
-    else
-      table.insert(names, arg)
-    end
-  end
-  vim.pack.update(#names > 0 and names or nil, pack_opts)
-end, {
-  bang = true,
-  nargs = "*",
-  complete = function(arglead)
-    return vim.tbl_filter(
-      function(f) return f:find(arglead, 1, true) == 1 end,
-      { "--offline", "--target=version", "--target=lockfile" }
-    )
-  end,
-})
-
-vim.api.nvim_create_user_command("PackDel", function(opts)
-  local names = vim.split(opts.args, " ", { trimws = true })
-  vim.pack.del(names, { force = opts.bang })
-end, {
-  bang = true,
-  nargs = "+",
-})
-
 vim.api.nvim_create_user_command("LspInfo", "checkhealth vim.lsp", {
   desc = "Show LSP Info",
 })
